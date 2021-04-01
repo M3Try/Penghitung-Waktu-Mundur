e0c0538e24834827282cd6a850cb6d8075742d41
dagowaksdev <try.nothing@gmail.com>
2021-04-01 11:09:06 +0700

    Next Kommit

---
+++ myscript.js
@@ -0,0 +1,24 @@
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var seconds = Math.floor((t / 1000) % 60);
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+/**
+ * Created by dagowaksdev.
+ *
+ * @param {String}  type  The type of adjustment.
+ * @param {Number}  value The number.
+ * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+ * @returns {Number} The adjusted value.
+ */
